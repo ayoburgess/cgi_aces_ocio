303694bcd8640bd2ceeb6d28f5f00bf0b162ee81
Ayo Burgess <ayo.burgess@gmail.com>
2016-07-21 08:34:21 -0400

    Fixed output lut paths

--- bake_luts.sh
+++ bake_luts.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ociobakelut --iconfig config.ocio --inputspace "ACES - ACES2065-1" --shaperspace "Utility - Log2 48 nits Shaper" --shapersize 4096 --outputspace "Utility - Input - Linear (sRGB) -> Output - Rec.709" --cubesize 64 --format houdini --v houdini/linear_sRGB_to_ACES_Rec709_output_transform.lut
-ociobakelut --iconfig config.ocio --inputspace "ACES - ACES2065-1" --shaperspace "Utility - Log2 48 nits Shaper" --shapersize 4096 --outputspace "Utility - ACES - ACEScg -> Output - Rec.709" --cubesize 64 --format houdini --v houdini/linear_ACEScg_to_ACES_Rec709_output_transform.lut
+ociobakelut --iconfig config.ocio --inputspace "ACES - ACES2065-1" --shaperspace "Utility - Log2 48 nits Shaper" --shapersize 4096 --outputspace "Utility - Input - Linear (sRGB) -> Output - Rec.709" --cubesize 64 --format houdini --v houdini/linear_sRGB_to_ACES_Rec709_output.lut
+ociobakelut --iconfig config.ocio --inputspace "ACES - ACES2065-1" --shaperspace "Utility - Log2 48 nits Shaper" --shapersize 4096 --outputspace "Utility - ACES - ACEScg -> Output - Rec.709" --cubesize 64 --format houdini --v houdini/ACEScg_to_ACES_Rec709_output.lut
